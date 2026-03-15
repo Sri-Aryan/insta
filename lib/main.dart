c52d2276c6d7fe8bd20c5fcd92c46b9d1e05e820
Aryan Srivastava <143885879+Sri-Aryan@users.noreply.github.com>
2026-03-16 01:46:47 +0530

    chore

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,6 @@ import 'package:insta/screens/home_screen.dart';
 
 void main() {
   runApp(
-    // ProviderScope is absolutely mandatory at the root of the app
-    // for Riverpod state management to function.
     const ProviderScope(
       child: InstagramCloneApp(),
     ),
@@ -19,7 +17,7 @@ class InstagramCloneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Instagram Pixel-Perfect Clone',
-      debugShowCheckedModeBanner: false, // Essential for that production feel
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         scaffoldBackgroundColor: Colors.white,
@@ -29,7 +27,6 @@ class InstagramCloneApp extends StatelessWidget {
           iconTheme: IconThemeData(color: Colors.black),
           titleTextStyle: TextStyle(color: Colors.black, fontSize: 20),
         ),
-        // Adding splash factory to match iOS behavior if testing on Android
         splashFactory: NoSplash.splashFactory,
         highlightColor: Colors.transparent,
       ),
