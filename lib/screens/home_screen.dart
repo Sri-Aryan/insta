fdeb3a4ae27c7914e5c82793ef61f26d2227c4f1
Aryan Srivastava <143885879+Sri-Aryan@users.noreply.github.com>
2026-03-16 19:31:16 +0530

    added home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:insta/widgets/story_stray.dart';
+import '../providers/feed_provider.dart';
+import '../widgets/post_card.dart';
+import '../widgets/shimmer.dart';
+
+class HomeFeedScreen extends ConsumerStatefulWidget {
+  const HomeFeedScreen({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<HomeFeedScreen> createState() => _HomeFeedScreenState();
+}
+
+class _HomeFeedScreenState extends ConsumerState<HomeFeedScreen> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  void _onScroll() {
+    if (_scrollController.position.pixels >=
+        _scrollController.position.maxScrollExtent - 1200) {
+      ref.read(feedProvider.notifier).fetchMore();
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final feedState = ref.watch(feedProvider);
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Text('Instagram', style: TextStyle(fontFamily: 'Billabong', fontSize: 32, color: Colors.black)),
+        actions: [
+          IconButton(icon: const Icon(Icons.favorite_border, color: Colors.black), onPressed: () {}),
+          IconButton(icon: const Icon(Icons.send, color: Colors.black), onPressed: () {}),
+        ],
+      ),
+      body: feedState.isLoading
+          ? Column(
+            children: [
+              //StoryTray(),
+              InstagramShimmer(),
+            ],
+          )
+          : RefreshIndicator(
+        onRefresh: () async => ref.refresh(feedProvider),
+        child: CustomScrollView(
+          controller: _scrollController,
+          slivers: [
+             SliverToBoxAdapter(child: StoryTray()),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                  if (index == feedState.posts.length) {
+                    return feedState.isFetchingMore
+                        ? const Padding(padding: EdgeInsets.all(16.0), child: Center(child: CircularProgressIndicator()))
+                        : const SizedBox.shrink();
+                  }
+                  final post = feedState.posts[index];
+                  return PostCard(post: post);
+                },
+                childCount: feedState.posts.length + 1,
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Image.asset("tabbar.png"),
+    );
+  }
+}
